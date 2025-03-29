13cd486c6eb262f899100e6043ab1196a8d93654
jeevan <jkoottkaran@gmail.com>
2025-03-29 10:52:10 +0530

    message

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,9 @@
 
+
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://13.210.229.137', // local
   //baseURL: 'http://13.210.229.137', // live
   headers: { 'Content-Type': 'application/json' },
 });
